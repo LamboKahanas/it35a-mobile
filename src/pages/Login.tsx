9731526314208f751031145b6456ad414a284614
LamboKahanas <lentysage@gmail.com>
2026-04-06 08:12:38 +0800

    new

---
+++ src/pages/Login.tsx
@@ -0,0 +1,28 @@
+import { IonPage, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonButton, useIonRouter } from "@ionic/react";
+
+const Login: React.FC = () => {
+    const navigation = useIonRouter();
+
+    const dologin = () => {
+        navigation.push('/app', 'forward', 'replace');
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonButtons>
+                        <IonMenuButton></IonMenuButton>
+                    </IonButtons>
+                    <IonTitle>Login</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <IonButton expand="full" onClick={() => dologin}>
+                    Login
+                </IonButton>
+
+
+            </IonContent>
+        </IonPage>
+    );
+}
+export default Login;
